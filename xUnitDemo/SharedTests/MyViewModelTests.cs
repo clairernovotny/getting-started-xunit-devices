349c2aebd30dee145faa0eb13275d06644668c85
Oren Novotny <oren@novotny.org>
2016-04-17 15:01:29 -0400

    Add first test

---
+++ xUnitDemo/SharedTests/MyViewModelTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SomeSharedLogic;
+using Xunit;
+
+namespace SharedTests
+{
+    public class MyViewModelTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            // arrange
+            var vm = new MyViewModel();
+
+            // act
+            var result = vm.Add(2, 3);
+
+            Assert.Equal(5, result);  
+        }
+    }
+}
