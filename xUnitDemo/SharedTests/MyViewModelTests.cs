ef60d6623ba360f909d89714c3883e02046c6886
Oren Novotny <oren@novotny.org>
2016-04-17 15:04:43 -0400

    Add theory

--- xUnitDemo/SharedTests/MyViewModelTests.cs
+++ xUnitDemo/SharedTests/MyViewModelTests.cs
@@ -17,7 +17,22 @@ namespace SharedTests
             // act
             var result = vm.Add(2, 3);
 
+            // assert
             Assert.Equal(5, result);  
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 3, 5)]
+        [InlineData(2, -5, -3)]
+        public void AddWorksForPositiveAndNegative(int a, int b, int result)
+        {
+            // arrange
+            var vm = new MyViewModel();
+            // act
+            var actual = vm.Add(a, b);
+            // assert
+            Assert.Equal(result, actual);
         }
     }
 }
