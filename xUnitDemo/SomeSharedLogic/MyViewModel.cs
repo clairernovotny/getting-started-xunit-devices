6be6176b9d5db2f906fb2fb0c68dfdf6cbfe222c
Oren Novotny <oren@novotny.org>
2016-04-17 14:14:39 -0400

    Initial add of projects

---
+++ xUnitDemo/SomeSharedLogic/MyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SomeSharedLogic
+{
+    public class MyViewModel
+    {
+        public int Add(int a, int b)
+        {
+            return a + Math.Abs(b);
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
